804715fc25d22c88e2e667c5ee0ab3ac260dcc4c
hiral panchal <Ketan-2005>
2026-01-08 20:12:41 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,33 @@ import "jsvectormap/dist/jsvectormap.css";
 import "flatpickr/dist/flatpickr.min.css";
 import "@/css/style.css";
 import React, { useEffect, useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import { UserProvider } from "./context/UserContext";
+import * as Ably from "ably";
+import { AblyProvider, ChannelProvider } from "ably/react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const client = new Ably.Realtime({
+    key: "hOKf0g.MYq_gA:vBCLEgU6f7tHCgiFfB0iRLYGFVoYAeYgjZMtoe2eULQ",
+  });
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>{children}</body>
+      <script src="https://app.unpkg.com/@rdkit/rdkit@2025.3.3-1.0.0/files/dist/RDKit_minimal.js"></script>
+      <body suppressHydrationWarning={true}>
+        <SessionProvider>
+          <UserProvider>
+            <AblyProvider client={client}>
+              <ChannelProvider channelName="chat-demo1">
+                {children}
+              </ChannelProvider>
+            </AblyProvider>
+          </UserProvider>
+        </SessionProvider>        
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import Index from "@/components/Dashboard";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title:
+    "next.js e-commerce dashboard | tailadmin - next.js dashboard template",
+  description: "this is next.js home for tailadmin dashboard template",
+  //   title:
+  //   "protein bind",
+  // description: "a leading research platform for drug discovery, this is a desc for pb",
+};
+
 export default function Home() {
   return (
     <>
-      <div className="h-screen">
-        <p>Default Layout</p>
-      </div>
+      <DefaultLayout>
+        <Index />
+      </DefaultLayout>
+      {/* <div className="h-screen">
+        <p>default layout</p>
+      </div> */}
+      {/* <DefaultLayout>
+        <p>default layout, hello dashboard | home page</p>
+      </DefaultLayout> */}
     </>
   );
 }
